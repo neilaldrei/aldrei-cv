afc36784651cb53fc331880fbcdcf0b1048bab12
Neil Aldrei <neilaldreiii@gmail.com>
2022-09-29 14:54:08 +0800

    wip: added skills

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="skills f-col center-text">
-        <div class="social-media-item" v-for="(link, i) in links" :key="i">
-            <a :href="link.url" target="_blank" :style="`color: ${link.color};`">
-                <i :class="`mdi ${link.icon} mdi-36px icon-hover`"></i>
-            </a>
+        <div class="social-media-item" v-for="(skill, i) in skills" :key="i">
+            <div :style="`color: ${skill.avatarColor};`">
+                <i :class="`mdi ${skill.icon} mdi-36px icon-hover`"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -15,6 +15,6 @@
     let skills = ref([]);
     
     onMounted ( async () => {
-        skills.value = await svc.getDocuments('skills', 5, 'yrsExp', 'desc');
+        skills.value = await svc.getDocuments('skills', 10, 'progress', 'desc');
     })
 </script>
--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -2,7 +2,7 @@
     <div class="social-media f-col center-text">
         <div class="social-media-item" v-for="(link, i) in links" :key="i">
             <a :href="link.url" target="_blank" :style="`color: ${link.color};`">
-                <i :class="`mdi ${link.icon} mdi-36px icon-hover`"></i>
+                <i :class="`mdi ${link.icon} mdi-36px`"></i>
             </a>
         </div>
     </div>
