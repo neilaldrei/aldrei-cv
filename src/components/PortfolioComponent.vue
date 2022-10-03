4854d86cbdd1a1d0583b3bb2c9f02db7f4fe9a30
Neil Aldrei <neilaldreiii@gmail.com>
2022-10-03 17:55:20 +0800

    wip: portfolio

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -1,68 +1,27 @@
 <template>
     <div class="portfolio">
-        portfolio
-        <div class="projects" v-for="(n, index) in 9" :key="index">
-            <div class="project"></div>
+        <div class="projects" v-for="(project, index) in projects" :key="index">
+            <div class="project">
+                <img :src="project.image" alt="">
+            </div>
+        </div>
+        
+        <div class="actions center-text">
+            <button role="button" class="btn btn-text">
+                Show all
+                <span class="btn-line"></span>
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
-    let webApps = [
-        {
-            title: "MAADDS Enterprise",
-            description: "A promotion website for NCF Students that will be used to promote their product for their thesis.",
-            image: "../assets/images/maadds.webp",
-            url: "https://maaddsenterprise.firebaseapp.com/",
-            deployed: "JUNE 2018",
-        },
-        {
-            title: "Triton Swim Club",
-            description: "A Web App for promoting, managing athletes and marketplace for their swimming equipments.",
-            image: "../assets/images/triton.webp",
-            url: "http://triton-swim-club.herokuapp.com/",
-            deployed: "OCT 2018",
-        },
-        {
-            title: "Anchovies Lumpia Express",
-            description: "A website for NCF students to promote their project.",
-            image: "../assets/images/anchovies.webp",
-            url: "https://anchovies-lumpia-enterprise.firebaseapp.com/",
-            deployed: "NOV 2019",
-        },
-        {
-            title: "NCF CBA Annex",
-            description: "A promotion website to showcase the new NCF Annex building for the CBA Students.",
-            image: "../assets/images/ncf-annex.webp",
-            url: "https://ncf-cba-annex.firebaseapp.com/",
-            deployed: "NOV 2019",
-        },
-        {
-            title: "JTTNet Messenger",
-            description: "A messaging app to manage payment status of applicant's reference number",
-            image: "../assets/images/jttnet.webp",
-            url: "http://flippy-dippy.herokuapp.com/",
-            deployed: "DEC 2019",
-        },
-        {
-            title: "Starshop",
-            description: "A web & mobile app for online selling and other delivery services.",
-            image: "../assets/images/shoppers.webp",
-            url: "http://nshop.herokuapp.com/",
-            deployed: "JULY 2020",
-        },
-        {
-            title: "JJ AQUAFRANCIA",
-            description: "A web app for payroll and taking customer orders.",
-            image: "../assets/images/ss.webp",
-            url: "http://jjaquafrancia.herokuapp.com/",
-            deployed: "AUGUST 2020",
-        },
-    ];
+    import svc from '@/common/firebase.service';
+    import { onMounted, ref } from 'vue';
 
-    console.log(webApps);
+    let projects = ref([]);
+
+    onMounted ( async () => {
+        projects.value = await svc.getDocuments('projects', 3, 'deployed', 'desc');
+    })
 </script>
-
-<style>
-
-</style>
